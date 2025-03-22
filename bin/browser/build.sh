411c150dafdf4055d491514d8ec76dbd1ea20e42
Arno van Wouwe <arno@vanwouwe.nl>
2025-03-22 16:36:32 +0100

    build script for the plugin

---
+++ bin/browser/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BUILD_ROOT="/tmp/citadel-$(uuidgen)"
+OUTPUT_PKG="citadel-plugin"
+
+rm -rf $OUTPUT_PKG
+mkdir $BUILD_ROOT
+
+# Copy the files to the directory structure
+cd ../..
+cp *.js *.json $BUILD_ROOT
+cp -r utils gui blacklist $BUILD_ROOT
+
+cd ./bin/browser/
+mv $BUILD_ROOT $OUTPUT_PKG
+cd $OUTPUT_PKG
+zip -r ../$OUTPUT_PKG.zip .
