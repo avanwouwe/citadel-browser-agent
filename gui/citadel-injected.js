fb7497428e0979278b2437d596bc27311594fa73
Arno van Wouwe <arno@vanwouwe.nl>
2025-04-04 08:35:15 +0200

    bugfix : do not send event listener events, as they cannot be serialized

--- gui/citadel-injected.js
+++ gui/citadel-injected.js
@@ -13,12 +13,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     document.addEventListener('click', function(event) {
-        chrome.runtime.sendMessage({type: "user-interaction", event: shallowClone(event)});
+        chrome.runtime.sendMessage({type: "user-interaction"});
     }, true);
 
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
-            chrome.runtime.sendMessage({type: "user-interaction", event: shallowClone(event)});
+            chrome.runtime.sendMessage({type: "user-interaction"});
         }
     }, true);
 
