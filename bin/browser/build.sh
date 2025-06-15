9980e3d892db29bec532e414e82432ac7639b3b7
Arno van Wouwe <arno@vanwouwe.nl>
2025-06-15 09:19:38 +0200

    make build script more robust

--- bin/browser/build.sh
+++ bin/browser/build.sh
@@ -4,14 +4,14 @@ BUILD_ROOT="/tmp/citadel-$(uuidgen)"
 OUTPUT_PKG="citadel-plugin"
 
 rm -rf $OUTPUT_PKG
-mkdir $BUILD_ROOT
+mkdir "$BUILD_ROOT"
 
 # Copy the files to the directory structure
 cd ../..
-cp *.js *.json $BUILD_ROOT
-cp -r utils gui blacklist $BUILD_ROOT
+cp ./*.js ./*.json "$BUILD_ROOT"
+cp -r utils gui blacklist "$BUILD_ROOT"
 
-cd ./bin/browser/
-mv $BUILD_ROOT $OUTPUT_PKG
-cd $OUTPUT_PKG
-zip -r ../$OUTPUT_PKG.zip .
+cd ./bin/browser/ || exit
+mv "$BUILD_ROOT" $OUTPUT_PKG
+cd "$OUTPUT_PKG" || exit
+zip -r ../"$OUTPUT_PKG".zip .
