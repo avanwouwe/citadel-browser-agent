6ace7382025b2ca84e8322378c41f34401bb1e56
Arno van Wouwe <arno@vanwouwe.nl>
2025-08-03 22:21:14 +0200

    cleaning up to make PasswordVault more readable

--- utils/trust/password-vault.js
+++ utils/trust/password-vault.js
@@ -2,14 +2,16 @@ class PasswordVault {
 
     static OTHER_SYSTEM = "[other-system]"
 
-    static async detectReuse(username, password, system) {
-        const passwordReuse = []
+    static #passwords
+    static salt
+    static prehashSalt
 
-        const salt = PasswordVault.#salts.bcrypt
-        if (!salt) return passwordReuse
+    static async detectReuse(username, password, system) {
+        if (! PasswordVault.salt || ! PasswordVault.prehashSalt) return console.warn("hash was no defined")
+        if (password.salt !== PasswordVault.prehashSalt) return console.warn("password was prehashed with incorrect salt")
 
         const isProtectedSystem = AccountTrust.checkFor(username, system)
-        const passwordHash = await Bcrypt.hash(password, salt)
+        const passwordHash = await Bcrypt.hash(password.hash, PasswordVault.salt)
         const accountKey = JSON.stringify({ u: username, s: system })
 
         const matchingPasswords = PasswordVault.#passwords[passwordHash] ?? {}
@@ -23,6 +25,7 @@ class PasswordVault {
             }
         }
 
+        const passwordReuse = []
         for (const acct of Object.values(matchingPasswords)) {
             if (acct.username !== username || acct.system !== system) {
                 passwordReuse.push({ username, system })
@@ -33,7 +36,7 @@ class PasswordVault {
             passwordReuse.push({ username, system: PasswordVault.OTHER_SYSTEM })
         }
 
-        return passwordReuse
+        return passwordReuse.length > 0 ? passwordReuse : undefined
     }
 
     static async purge() {
@@ -41,7 +44,6 @@ class PasswordVault {
 
         const cutoff = config.account.retentionDays * ONE_DAY
         const now = Date.now()
-        const globalSalt = PasswordVault.#salts.bcrypt
 
         for (const [passwordHash, accounts] of Object.entries(PasswordVault.#passwords)) {
             if (passwordHash === "isDirty") continue
@@ -53,45 +55,30 @@ class PasswordVault {
                 }
             }
 
-            if (Object.keys(accounts).length === 0 || Bcrypt.getSalt(passwordHash) !==  globalSalt) {
+            if (Object.keys(accounts).length === 0 || Bcrypt.getSalt(passwordHash) !==  PasswordVault.salt) {
                 delete PasswordVault.#passwords[passwordHash]
             }
         }
     }
 
-    static #passwords = new PersistentObject("passwords").value()
-    static #salts = new PersistentObject("password-salts").value()
-    static prehashSalt
-
     static {
         // load the state, re-initializing everything if something looks corrupt
-        const loadSalts = new PersistentObject("password-salts").ready().then(store => PasswordVault.#salts = store.value())
-        const loadPasswords = new PersistentObject("passwords").ready().then(store => PasswordVault.#passwords = store.value())
+        let salts
+        const loadSalts = new PersistentObject("password-salts").ready().then(obj => salts = obj.value())
+        const loadPasswords = new PersistentObject("passwords").ready().then(obj => PasswordVault.#passwords = obj.value())
 
-        Promise.all([loadSalts, loadPasswords]).then(() => {
-            const salts = PasswordVault.#salts
+        Promise.all([loadSalts, loadPasswords]).then(async () => {
             if (!salts.bcrypt || !salts.pbkdf2) {
-                debug("initializing password vault", salts.bcrypt,salts.pbkdf2)
+                debug("initializing password vault")
+
                 Bcrypt.genSalt().then(salt => salts.bcrypt = salt)
                 salts.pbkdf2 = PBKDF2.toBase64(PBKDF2.genSalt())
 
-                const passwordStorage = new PersistentObject("passwords")
-                passwordStorage.clear()
-                PasswordVault.#passwords = passwordStorage.value()
+                PasswordVault.#passwords.clear()
             }
 
+            PasswordVault.salt = salts.bcrypt
             PasswordVault.prehashSalt = salts.pbkdf2
         })
-
-        const handler = (message, sender, sendResponse) => {
-            if (sender.id !== chrome.runtime.id) return
-
-            if (message.type === "CheckPasswordReuse") {
-                const passwordReuse = PasswordVault.detectReuse(message.report.username, message.report.password, sender.origin)
-                sendResponse(passwordReuse)
-            }
-        }
-
-        chrome.runtime.onMessage.addListener(handler)
     }
 }
