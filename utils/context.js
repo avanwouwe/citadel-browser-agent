f6fa449fbe12c4a08348d460999fbe5c65d8bef0
Arno van Wouwe <arno@vanwouwe.nl>
2025-09-15 21:59:41 +0200

    Firefox bugfix : was mis-detecting the context

--- utils/context.js
+++ utils/context.js
@@ -1,13 +1,22 @@
 class Context {
     static isServiceWorker() {
-        return typeof window === "undefined" && typeof chrome !== "undefined" && typeof chrome.runtime !== "undefined"
+        return (typeof window === 'undefined' && typeof importScripts === 'function')
     }
 
     static isContentScript() {
-        return typeof window !== "undefined" && typeof chrome !== "undefined" && typeof chrome.runtime !== "undefined"
+        if (this.isServiceWorker()) return false
+
+        const isExtensionPage =
+            typeof location !== 'undefined' &&
+            (location.protocol === 'chrome-extension:' || location.protocol === 'moz-extension:')
+        const hasExtensionId =
+            (typeof chrome !== 'undefined' && chrome.runtime && chrome.runtime.id) ||
+            (typeof browser !== 'undefined' && browser.runtime && browser.runtime.id);
+
+        return hasExtensionId && !isExtensionPage
     }
 
     static isPageScript() {
-        return typeof window !== "undefined" && (typeof chrome === "undefined" || typeof chrome.runtime === "undefined")
+        return ! this.isServiceWorker() && ! this.isContentScript()
     }
 }
