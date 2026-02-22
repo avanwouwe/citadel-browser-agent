bdb41adf3276a4cdf7fbf3f20155bcd816212cd4
Arno van Wouwe <arno@vanwouwe.nl>
2026-02-22 16:09:36 +0100

    integrate Opera-specific permissions, and make more readable

--- utils/trust/extension/extension.js
+++ utils/trust/extension/extension.js
@@ -4,243 +4,58 @@ class Extension {
 
     static Permissions = class {
         static #permissions = {
-            "accessibilityFeatures.modify": {
-                risk: undefined
-            },
-            "accessibilityFeatures.read": {
-                risk: undefined
-            },
-            "activeTab": {
-                risk: 1
-            },
-            "alarms": {
-                risk: 2
-            },
-            "audio": {
-                risk: undefined
-            },
-            "background": {
-                risk: 1
-            },
-            "bookmarks": {
-                risk: 2
-            },
-            "browsingData": {
-                risk: 3
-            },
-            "certificateProvider": {
-                risk: undefined
-            },
-            "clipboardRead": {
-                risk: 4
-            },
-            "clipboardWrite": {
-                risk: 2
-            },
-            "contentSettings": {
-                risk: undefined
-            },
-            "contextMenus": {
-                risk: 2
-            },
-            "cookies": {
-                risk: 4
-            },
-            "debugger": {
-                risk: 4
-            },
-            "declarativeContent": {
-                risk: 3
-            },
-            "declarativeNetRequest": {
-                risk: 3
-            },
-            "declarativeNetRequestWithHostAccess": {
-                risk: undefined
-            },
-            "declarativeNetRequestFeedback": {
-                risk: undefined
-            },
-            "desktopCapture": {
-                risk: 3
-            },
-            "dns": {
-                risk: undefined
-            },
-            "documentScan": {
-                risk: undefined
-            },
-            "downloads": {
-                risk: 2
-            },
-            "downloads.open": {
-                risk: undefined
-            },
-            "downloads.ui": {
-                risk: undefined
-            },
-            "enterprise.deviceAttributes": {
-                risk: undefined
-            },
-            "enterprise.hardwarePlatform": {
-                risk: undefined
-            },
-            "enterprise.networkingAttributes": {
-                risk: undefined
-            },
-            "enterprise.platformKeys": {
-                risk: undefined
-            },
-            "favicon": {
-                risk: undefined
-            },
-            "fileBrowserHandler": {
-                risk: undefined
-            },
-            "fileSystemProvider": {
-                risk: 3
-            },
-            "fontSettings": {
-                risk: undefined
-            },
-            "gcm": {
-                risk: undefined
-            },
-            "geolocation": {
-                risk: undefined
-            },
-            "history": {
-                risk: 3
-            },
-            "identity": {
-                risk: 1
-            },
-            "identity.email": {
-                risk: undefined
-            },
-            "idle": {
-                risk: 2
-            },
-            "loginState": {
-                risk: undefined
-            },
-            "management": {
-                risk: 2
-            },
-            "nativeMessaging": {
-                risk: 2
-            },
-            "notifications": {
-                risk: 1
-            },
-            "offscreen": {
-                risk: 2
-            },
-            "pageCapture": {
-                risk: 4
-            },
-            "platformKeys": {
-                risk: undefined
-            },
-            "power": {
-                risk: 3
-            },
-            "printerProvider": {
-                risk: undefined
-            },
-            "printing": {
-                risk: undefined
-            },
-            "printingMetrics": {
-                risk: undefined
-            },
-            "privacy": {
-                risk: 3
-            },
-            "processes": {
-                risk: undefined
-            },
-            "proxy": {
-                risk: 4
-            },
-            "readingList": {
-                risk: undefined
-            },
-            "runtime": {
-                risk: undefined
-            },
-            "scripting": {
-                risk: 4
-            },
-            "search": {
-                risk: undefined
-            },
-            "sessions": {
-                risk: undefined
-            },
-            "sidePanel": {
-                risk: 2
-            },
-            "storage": {
-                risk: 2
-            },
-            "system.cpu": {
-                risk: undefined
-            },
-            "system.display": {
-                risk: undefined
-            },
-            "system.memory": {
-                risk: undefined
-            },
-            "system.storage": {
-                risk: undefined
-            },
-            "tabCapture": {
-                risk: 3
-            },
-            "tabGroups": {
-                risk: undefined
-            },
-            "tabs": {
-                risk: 3
-            },
-            "topSites": {
-                risk: undefined
-            },
-            "tts": {
-                risk: undefined
-            },
-            "ttsEngine": {
-                risk: undefined
-            },
-            "unlimitedStorage": {
-                risk: 3
-            },
-            "userScripts": {
-                risk: undefined
-            },
-            "vpnProvider": {
-                risk: undefined
-            },
-            "wallpaper": {
-                risk: undefined
-            },
-            "webAuthenticationProxy": {
-                risk: undefined
-            },
-            "webNavigation": {
-                risk: 3
-            },
-            "webRequest": {
-                risk: 4
-            },
-            "webRequestBlocking": {
-                risk: 1
-            },
-            "<all_urls>": {
-                risk: 4
-            }
+            "<all_urls>":                           { risk: 4 },
+            "clipboardRead":                        { risk: 4 },
+            "cookies":                              { risk: 4 },
+            "debugger":                             { risk: 4 },
+            "pageCapture":                          { risk: 4 },
+            "proxy":                                { risk: 4 },
+            "scripting":                            { risk: 4 },
+            "webRequest":                           { risk: 4 },
+            "browsingData":                         { risk: 3 },
+            "declarativeContent":                   { risk: 3 },
+            "declarativeNetRequest":                { risk: 3 },
+            "desktopCapture":                       { risk: 3 },
+            "fileSystemProvider":                   { risk: 3 },
+            "history":                              { risk: 3 },
+            "power":                                { risk: 3 },
+            "privacy":                              { risk: 3 },
+            "tabCapture":                           { risk: 3 },
+            "tabs":                                 { risk: 3 },
+            "unlimitedStorage":                     { risk: 3 },
+            "webNavigation":                        { risk: 3 },
+            "alarms":                               { risk: 2 },
+            "bookmarks":                            { risk: 2 },
+            "clipboardWrite":                       { risk: 2 },
+            "contextMenus":                         { risk: 2 },
+            "downloads":                            { risk: 2 },
+            "idle":                                 { risk: 2 },
+            "management":                           { risk: 2 },
+            "nativeMessaging":                      { risk: 2 },
+            "offscreen":                            { risk: 2 },
+            "sidePanel":                            { risk: 2 },
+            "storage":                              { risk: 2 },
+            "activeTab":                            { risk: 1 },
+            "background":                           { risk: 1 },
+            "identity":                             { risk: 1 },
+            "notifications":                        { risk: 1 },
+            "webRequestBlocking":                   { risk: 1 },
+        // Opera-specific
+            "browserOperatorPrivate":               { risk: 3 },
+            "operaAccountPrivate":                  { risk: 3 },
+            "operaBrowserPrivate":                  { risk: 3 },
+            "operaIdentityPrivate":                 { risk: 3 },
+            "shodanPrivate":                        { risk: 3 },
+            "tabsPrivate":                          { risk: 3 },
+            "browserSidebarPrivate":                { risk: 2 },
+            "localModelsPrivate":                   { risk: 2 },
+            "statsPrivate":                         { risk: 2 },
+            "workspacesPrivate":                    { risk: 2 },
+            "browserSplitViewPrivate":              { risk: 1 },
+            "feedbackPopupPrivate":                 { risk: 1 },
+            "liveWallpaperMetricsPrivate":          { risk: 1 },
+            "palette":                              { risk: 1 },
+            "startpagePrivate":                     { risk: 1 },
         }
 
         static {
